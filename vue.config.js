e10d72cb6fd16e8edf84649b9a874249557f021a
Ferry Anggriawan <ferrytailsekai@gmail.com>
2019-03-24 03:12:26 +0700

    add docs folder build

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  pwa: {
+    name: 'Ferry Anggriawan'
+  },
+
+  publicPath: undefined,
+  outputDir: 'docs',
+  assetsDir: undefined,
+  runtimeCompiler: true,
+  productionSourceMap: false,
+  parallel: undefined,
+  css: {
+    modules: true
+  }
+}
