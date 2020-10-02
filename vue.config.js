fc6e55c38c82dc500d2694d7fd6ab67805f58fd8
Ferry Anggriawan <ferrya1997@gmail.com>
2020-10-03 01:21:07 +0700

    init dev

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,5 @@
 module.exports = {
-  pwa: {
-    name: "Ferry Anggriawan"
-  },
-
-  publicPath: process.env.NODE_ENV === "production" ? "docs/" : "/",
-  outputDir: "docs",
-  assetsDir: "",
-  runtimeCompiler: true,
-  productionSourceMap: false,
-  css: {
-    requireModuleExtension: true
-  }
-};
+  "transpileDependencies": [
+    "vuetify"
+  ]
+}
